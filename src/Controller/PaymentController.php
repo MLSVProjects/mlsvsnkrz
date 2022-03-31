79a7d8369dbff8374dee35d6f8e16af3b16302d1
Merwane Zioui <merwane.zioui@gmail.com>
2022-03-31 19:34:34 +0200

    fix(cart): Cart Service in Payment
    Remplacement of raw datas by Cart Service.

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\CartService;
 use App\Service\OrderService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,37 +21,25 @@ class PaymentController extends AbstractController
 	}
 
 	#[Route('/checkout', name: 'checkout')]
-	public function checkout(Request $request, StripeApiService $stripeApiService): Response
+	public function checkout(Request $request, StripeApiService $stripeApiService, CartService $cartService): Response
 	{
 		$sessionCart = $request->getSession();
 
-		$cart = [
-			[
-				'product_id'=>1,
-				'quantity'=>2
-			],
-			[
-				'product_id'=>2,
-				'quantity'=>1
-			]
-		];
-
-		$sessionCart->set('cart', $cart);
-
-		$session = $stripeApiService->createCheckout($cart);
+		$session = $stripeApiService->createCheckout($cartService->getCart());
 
 		return $this->redirect($session->url, 303);
 
 	}
 
 	#[Route('/success', name: 'success_url')]
-	public function successUrl(StripeApiService $stripeApiService, Request $request, OrderService $orderService): Response
+	public function successUrl(StripeApiService $stripeApiService, Request $request, OrderService $orderService, CartService $cartService): Response
 	{
 		$sessionCart = $request->getSession();
 		$cart = $sessionCart->get('cart');
 		$session_id = $request->get('session_id');
 		$session = $stripeApiService->getCheckoutSession($session_id);
 		$orderService->createOrder($cart, $session);
+		$cartService->resetCart();
 		return $this->render('payment/success.html.twig', [
 			'controller_name' => 'PaymentController',
 		]);
--- src/Controller/ProductDetailController.php
+++ src/Controller/ProductDetailController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,4 +17,31 @@ public function home() : Response
 return $this->render('product_detail.html.twig');
 }
 
+/**
+ * @Route("/add/{product_id}/{quantity}/", name="add_to_cart")
+ */
+public function addToCart($product_id, $quantity, CartService $cartService)
+{
+	$cartService->addToCart($product_id, $quantity);
+	//dd($key);
+	dd($cartService->getCart());
+}
+
+/**
+ * @Route("/remove/{product_id}/{quantity}/", name="remove_from_cart")
+ */
+public function removeFromCart($product_id, $quantity, CartService $cartService)
+{
+	$cartService->removeFromCart($product_id, $quantity);
+	dd($cartService->getCart());
+}
+
+/**
+ * @Route("/reset/", name="reset_cart")
+ */
+public function resetCart(CartService $cartService)
+{
+	$cartService->resetCart();
+	dd($cartService->getCart());
+}
 }
